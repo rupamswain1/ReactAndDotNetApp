42c1070699c466a7c68132cb912b1f1e601f3cd7
rupam swain <rupamswain1@gmail.com>
2023-04-04 00:21:32 +0530

    homepage removed from nav

--- clientApp/src/app/layout/App.tsx
+++ clientApp/src/app/layout/App.tsx
@@ -4,14 +4,23 @@ import { Container } from 'semantic-ui-react';
 
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import HomePage from '../../feature/home/HomePage';
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <div className="App">
-      <NavBar />
-      <Container style={{ marginTop: '7rem' }}>
-        <Outlet />
-      </Container>
+      {pathname === '/' ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: '7rem' }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </div>
   );
 }
