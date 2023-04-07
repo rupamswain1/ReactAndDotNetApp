998caf6ec1a9646588f0f46797289a8fdd9576e0
rupam swain <rupamswain1@gmail.com>
2023-04-07 23:41:02 +0530

    style change

--- clientApp/src/feature/activities/dashboard/ActivityListItem.tsx
+++ clientApp/src/feature/activities/dashboard/ActivityListItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Button, Item, Label } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import { Link, NavLink } from 'react-router-dom';
 import { useStore } from '../../../app/store/store';
 import { Activity } from '../../../app/model/Activity';
 interface Props {
@@ -12,40 +12,75 @@ const ActivityListItem = ({
   activity: { id, title, date, description, city, venue, category },
 }: Props) => {
   const {
-    activityStore: { deleteActivity },
+    activityStore: { deleteActivity, selectActivity },
   } = useStore();
 
   return (
-    <Item key={id}>
-      <Item.Content>
-        <Item.Header as="a">{title}</Item.Header>
-        <Item.Meta>{date}</Item.Meta>
-        <Item.Description>
-          <div>{description}</div>
-          <div>
-            {city},{venue}
-          </div>
-        </Item.Description>
-        <Item.Extra>
-          <Button
-            floated="right"
-            content="View"
-            color="blue"
-            // onClick={() => selectActivity(id)}
-            as={NavLink}
-            to={`${id}`}
-          />
-          <Button
-            floated="right"
-            content="Delete"
-            color="red"
-            onClick={() => deleteActivity(id)}
-          />
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image
+              size="tiny"
+              circular
+              src="https://source.unsplash.com/featured/300x201"
+            />
+            <Item.Content>
+              <Item.Header
+                onClick={() => selectActivity(id)}
+                as={NavLink}
+                to={`${id}`}
+              >
+                {title}
+              </Item.Header>
+              <Item.Description>Hosted by BOB</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment secondary>Attendees Go Here</Segment>
+      <Segment clearing>
+        <span>{description}</span>
+        <Button
+          floated="right"
+          content="View"
+          color="blue"
+          onClick={() => selectActivity(id)}
+          as={NavLink}
+          to={`${id}`}
+        />
+      </Segment>
+    </Segment.Group>
+    // <Item key={id}>
+    //   <Item.Content>
+    //     <Item.Header as="a">{title}</Item.Header>
+    //     <Item.Meta>{date}</Item.Meta>
+    //     <Item.Description>
+    //       <div>{description}</div>
+    //       <div>
+    //         {city},{venue}
+    //       </div>
+    //     </Item.Description>
+    //     <Item.Extra>
+    //       <Button
+    //         floated="right"
+    //         content="View"
+    //         color="blue"
+    //         // onClick={() => selectActivity(id)}
+    //         as={NavLink}
+    //         to={`${id}`}
+    //       />
+    //       <Button
+    //         floated="right"
+    //         content="Delete"
+    //         color="red"
+    //         onClick={() => deleteActivity(id)}
+    //       />
 
-          <Label basic content={category} />
-        </Item.Extra>
-      </Item.Content>
-    </Item>
+    //       <Label basic content={category} />
+    //     </Item.Extra>
+    //   </Item.Content>
+    // </Item>
   );
 };
 
